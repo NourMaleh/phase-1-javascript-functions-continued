bb0b611f834866bbc09ee6af97c1209bfc32725a
nour al maleh <nourmale7@gmail.com>
2022-12-06 22:24:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function mondayWork( argument = "go to the office") {
+    return "This Monday, I will " +  argument + "."}
+
+mondayWork( argument);
+
+
+function saturdayFun( argument = "roller-skate") {
+    return "This Saturday, I want to " +  argument + "!"}
+
+saturdayFun( argument);
+
+function wrapAdjective(whatever = "*") {
+    return function (special = "special") {
+        return `You are ${whatever}${special}${whatever}!`}}
+        
+        console.log(wrapAdjective("%")("a dedicated programmer"));
